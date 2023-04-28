3ad16de23d8afa5b5a134ee21086f98dc59e6963
ysureshbabu <Yelendi.SureshBabu@ghd.com>
2023-04-28 16:21:26 +0530

    API delete req

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -5,6 +5,7 @@ function CommentsPage() {
   const fetchComments = async () => {
     const response = await fetch("/api/comments");
     const data = await response.json();
+    console.log("hghjgjhg", data);
     setComments(data);
   };
   const submitComment = async () => {
@@ -17,6 +18,14 @@ function CommentsPage() {
     });
     const data = await response.json();
     console.log(data);
+  };
+  const deleteComment = async commentId => {
+    const response = await fetch(`/api/comments/${commentId}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    console.log(data);
+    fetchComments();
   };
   return (
     <>
@@ -31,6 +40,9 @@ function CommentsPage() {
         return (
           <div key={comment.id}>
             {comment.id}. {comment.text}
+            <button onClick={() => deleteComment(comment.id)}>
+              Delete Comment
+            </button>
           </div>
         );
       })}
