faeaf01ebff82eb791048a24178a290e0fbec5aa
ysureshbabu <Yelendi.SureshBabu@ghd.com>
2023-04-28 16:55:59 +0530

    APIs and Pre-rendering

--- pages/api/comments/[commentId].js
+++ pages/api/comments/[commentId].js
@@ -2,7 +2,7 @@ import { comments } from "../../../data/comments";
 export default function handler(req, res) {
   const { commentId } = req.query;
   if (req.method === "GET") {
-    const comment = comments.filter(
+    const comment = comments.find(
       comment => comment.id === parseInt(commentId)
     );
     res.status(200).json(comment);
