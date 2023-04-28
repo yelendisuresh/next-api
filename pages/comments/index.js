57bb90780ed3f8ce371eba7c7a7c715b7afdeea6
ysureshbabu <Yelendi.SureshBabu@ghd.com>
2023-04-28 15:48:12 +0530

    API POST Request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -1,13 +1,31 @@
 import { useState } from "react";
 function CommentsPage() {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
   const fetchComments = async () => {
     const response = await fetch("/api/comments");
     const data = await response.json();
     setComments(data);
   };
+  const submitComment = async () => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment: comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <>
+      <input
+        type="text"
+        value={comment}
+        onChange={e => setComment(e.target.value)}
+      />
+      <button onClick={submitComment}>Submit Comment</button>
       <button onClick={fetchComments}>Load comments</button>
       {comments.map(comment => {
         return (
