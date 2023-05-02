f92e67edd4522a7fe76ae8bde0537416afcd97cc
ysureshbabu <Yelendi.SureshBabu@ghd.com>
2023-05-02 11:12:33 +0530

    CSS Modules

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import styles from "../styles/About.module.css";
 const about = () => {
   return (
     <div>
-      <h2> about</h2>
+      <h2 className={styles.highlight}> about</h2>
       <button className="btn btn-success">Primary</button>
     </div>
   );
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "../styles/Contact.module.css";
 
 const contact = () => {
   return (
     <div>
-      <h2>Contact Page</h2>
+      <h2 className={styles.highlight}>Contact Page</h2>
     </div>
   );
 };
