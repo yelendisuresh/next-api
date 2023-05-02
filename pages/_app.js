0f7d7a4f52b11ac5b8ae8bfe0d7ed76fb9f47c55
ysureshbabu <Yelendi.SureshBabu@ghd.com>
2023-05-02 12:02:37 +0530

    css in js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "@/styles/globals.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary: "#355C7D",
+  },
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
