46fe837aa82807ff1434497b98bb9c8da754d7ac
ysureshbabu <Yelendi.SureshBabu@ghd.com>
2023-05-02 10:46:53 +0530

    Global styling with bootstrap

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <h2> about</h2>
+      <button className="btn btn-success">Primary</button>
+    </div>
+  );
+};
+
+export default about;
